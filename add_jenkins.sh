966e586d79737f965aaa52f620c2618116aa85c9
Torsten Kleiber <torsten.kleiber@web.de>
2016-10-31 23:55:24 +0100

    add java option for tls

--- add_jenkins.sh
+++ add_jenkins.sh
@@ -2,5 +2,6 @@ sudo yum -y install java
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum -y install jenkins
+sudo sed -i -e 's/JENKINS_JAVA_OPTIONS="-Djava.awt.headless=true"/JENKINS_JAVA_OPTIONS="-Djava.awt.headless=true -Dmail.smtp.starttls.enable=true"/g' /etc/sysconfig/jenkins
 sudo service jenkins start
 sudo chkconfig jenkins on
