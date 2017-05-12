59058f911b5f8828726fcb45479df2104d6c6f55
Torsten Kleiber <torsten.kleiber@web.de>
2017-05-12 22:13:27 +0200

    add docker-compose

--- config_docker.sh
+++ config_docker.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+sudo curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 echo create docker config
 sudo mkdir /etc/systemd/system/docker.service.d
 sudo touch /etc/systemd/system/docker.service.d/docker.conf
