a8810038250f72504026960c3dcbd420fd765b1e
Torsten Kleiber <torsten.kleiber@web.de>
2017-07-27 22:01:35 +0200

    Change to centos/7 vagrant box.

--- Vagrantfile
+++ Vagrantfile
@@ -24,7 +24,7 @@ Vagrant.configure(2) do |config|
   config.vm.synced_folder "C:\\shared\\virtual_storage\\jenkins_home", "/var/lib/jenkins", type: "nfs", owner: 994, group: 992, create: true
   # persistant storage for software
   config.vm.synced_folder "D:\\download", "/software", :mount_options => ["dmode=555","fmode=555"]
-  
+
   # virtualbox provider
   config.vm.provider "virtualbox" do |vb|
     # name in VirtualBox
--- add_jenkins.sh
+++ add_jenkins.sh
@@ -4,5 +4,6 @@ sudo yum -y install wget
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum -y install jenkins
+sudo yum -y install git
 sudo sed -i -e 's/JENKINS_JAVA_OPTIONS="-Djava.awt.headless=true"/JENKINS_JAVA_OPTIONS="-Djava.awt.headless=true -Dmail.smtp.starttls.enable=true"/g' /etc/sysconfig/jenkins
 sudo service jenkins start
