104513ece0fdbc8ebf08b883d6a38139f64e70b7
Torsten Kleiber <torsten.kleiber@web.de>
2017-08-18 21:00:36 +0200

    install current docker compose, add local scm folder as synced folder

--- config_docker.sh
+++ config_docker.sh
@@ -1,18 +1,5 @@
 #!/bin/sh
-
-echo install docker compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.15.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-echo create docker config
-sudo mkdir /etc/systemd/system/docker.service.d
-sudo touch /etc/systemd/system/docker.service.d/docker.conf
-echo '[Service]' | sudo tee /etc/systemd/system/docker.service.d/docker.conf > /dev/null
-echo 'ExecStart=' | sudo tee --append /etc/systemd/system/docker.service.d/docker.conf > /dev/null
-# Enable Docker Remote API, Insecure Registry, use devicemapper storage driver to set Basesize
-echo 'ExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --insecure-registry=localhost:5000 --storage-driver=devicemapper --storage-opt=dm.basesize=50G' | sudo tee --append /etc/systemd/system/docker.service.d/docker.conf > /dev/null
-# echo 'ExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --insecure-registry=localhost:5000' | sudo tee --append /etc/systemd/system/docker.service.d/docker.conf > /dev/null
-echo flush config changes
-sudo systemctl daemon-reload
-echo change PasswordAuthentication to yes
-sudo sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo service sshd restart
+sudo su
+curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+exit
