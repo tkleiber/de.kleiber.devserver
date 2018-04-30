fa72afab5ee89cec6fdc0f1dd84469a92ec7fac1
Torsten Kleiber <torsten.kleiber@web.de>
2018-04-30 22:48:27 +0200

    add user vagrant to group docker

--- config_docker.sh
+++ config_docker.sh
@@ -2,4 +2,5 @@
 sudo su
 curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker vagrant
 exit
